b7db6bd740c8b4305782c5c03b87913e7814ddd7
sabahat-123 <sabahat.parveen@imtspl.net>
2022-10-19 16:42:44 +0530

    first commit

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+const content = document.querySelectorAll('.content');
+const listItem = document.querySelectorAll('nav ul li');
 
+listItem.forEach((item,index)=>{
+         item.addEventListener('click',()=>{
+                hideAllContents();
+                hideAllItems();
+                item.classList.add('active');
+                content[index].classList.add('show');
+         });
+});
+function hideAllContents(){
+    content.forEach(content =>content.classList.remove('show'));
+}
+function hideAllItems(){
+    listItem.forEach(item =>item.classList.remove('active'));
+}
